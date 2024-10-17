9632ed2d3c0c09f6a9695bc1569dcc6905c48550
mrhrmant <patelgaming822@gmail.com>
2024-10-17 10:08:26 +0100

    basic logic

--- Stop watch/script.js
+++ Stop watch/script.js
@@ -2,33 +2,63 @@ let start = document.getElementById("btn");
 let timeValue = document.getElementById("set-second");
 let stopWatch = document.getElementById("stop-watch");
 let stop = document.getElementById("stop");
-let reset = document.getElementById("reset")
+let clear = document.getElementById("clear");
+// let ten=document.getElementById("ten");
+// let twenty=document.getElementById("twenty");
+// let minute=document.getElementById("minute");
+// let twoMin=document.getElementById("twomin");
+// testing
+let hidden = timeValue.getAttribute("hidden");
+function toggle() {
+
+    // let hidden2=start.getAnimations("hidden2")
+    if (hidden) {
+        timeValue.removeAttribute("hidden");
+
+    } else {
+        timeValue.setAttribute("hidden", "hidden")
+    }
+
+
+}
+
+
 
 start.addEventListener("click", () => {
+
     let time = timeValue.value;
+
     stopWatch.textContent = time;
     let sets = setInterval(function () {
         stopWatch.textContent = time;
         time--;
         stopWatch.innerHTML = `${time}`;
-        timeValue.value='0';
-        
-        
+        timeValue.value = '0';
+
+
         if (time === 0) {
             clearInterval(sets)
         }
-        
+
         stop.addEventListener("click", () => {
-            clearInterval(sets)
-
-
-        });
-        reset.addEventListener("click",()=>{
             clearInterval(sets);
-            stopWatch.innerHTML='0';
-            timeValue.value='0';
+            timeValue.removeAttribute("hidden");
+
+
+
+
         });
-        
+        clear.addEventListener("click", () => {
+            clearInterval(sets);
+            stopWatch.textContent = '0';
+            timeValue.value = '0';
+            
+            
+
+
+        });
+
+
 
     }, 1000);
 
